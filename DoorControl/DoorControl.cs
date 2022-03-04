4e48e10ee8925a00505ae0f73432019bc682f4eb
Cecilie Svangtun Nowak <201910286@post.au.dk>
2022-03-04 13:53:51 +0100

    t4t4

--- DoorControl/DoorControl.cs
+++ DoorControl/DoorControl.cs
@@ -69,7 +69,6 @@ namespace DoorControlProject
             {
                 case State.DoorClosing:
                     _doorState = State.DoorClosed;
-                    _doorState = State.DoorClosed;
                     break;
             }
         }
