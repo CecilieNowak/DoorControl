7d0cd4e90f592d787294fa0f01f49bace00b7ff2
Cecilie Svangtun Nowak <201910286@post.au.dk>
2022-03-07 16:48:32 +0100

    jenkins

---
+++ DoorControl/GitClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoorControlProject
+{
+    class GitClass
+    {
+        public GitClass()
+        {
+            Console.Write("");
+        }
+
+    }
+}
